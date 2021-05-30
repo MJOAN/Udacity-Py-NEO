ae5855ce10123ffc8844d0e9ffed3da5cc8d8ffe
Mariam Joan <royalton@gmail.com>
2021-05-30 10:21:51 -0700

    Create extract.py

---
+++ extract.py
@@ -0,0 +1,50 @@
+"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
+
+The `load_neos` function extracts NEO data from a CSV file, formatted as
+described in the project instructions, into a collection of `NearEarthObject`s.
+
+The `load_approaches` function extracts close approach data from a JSON file,
+formatted as described in the project instructions, into a collection of
+`CloseApproach` objects.
+
+The main module calls these functions with the arguments provided at the command
+line, and uses the resulting collections to build an `NEODatabase`.
+
+You'll edit this file in Task 2.
+"""
+import csv
+import json
+
+from models import NearEarthObject, CloseApproach
+
+######### Code References ###########################################################
+## 1. Vanina W, Udacity Mentor Board, https://knowledge.udacity.com/questions/465782
+## 2. Vanina W, Udacity Mentor Board, https://knowledge.udacity.com/questions/461587
+#####################################################################################
+
+def load_neos(neo_csv_path):
+    """Read near-Earth object information from a CSV file.
+
+    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
+    :return: A collection of `NearEarthObject`s.
+    """
+    neo_data = []
+    with open(neo_csv_path, 'r') as c:
+        reader = csv.DictReader(c)
+        for row in reader:
+            neo_data.append(NearEarthObject(row))  # 2  (removed **row with row got error)
+    return neo_data
+
+
+def load_approaches(cad_json_path):
+    """Read close approach data from a JSON file.
+    :param neo_csv_path: A path to a JSON file containing data about close approaches.
+    :return: A collection of `CloseApproach`'s.
+    """
+    cad_data = []
+    with open(cad_json_path, 'r') as j: 
+        row = json.load(j)
+        for approaches in row['data']:                       
+            approaches = dict(zip(row['fields'], approaches))    # 1
+            cad_data.append(CloseApproach(**approaches))    # 1  
+    return cad_data
